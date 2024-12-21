e6da7042b7c2b693302434ff846ae9431df7363d
QuanPham2005 <ptquan.k23tt@kontum.udn.vn>
2024-12-21 20:15:44 +0700

    [1] coffee part 3

---
+++ src/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const topMenu = document.getElementById('cf-top-menu')
+    const toggleMenu = document.getElementById('cf-toggle-top-menu')
+    
+    document.addEventListener('click', (e) => {
+        if (toggleMenu.contains(e.target)){
+            topMenu.classList.toggle('cf-expanded')
+            topMenu.classList.toggle('hidden')
+        } else {
+            if(topMenu.classList.contains('cf-expanded')){
+                topMenu.classList.remove('cf-expanded')
+                topMenu.classList.add('hidden')
+            }
+        }
+    })
+    })
